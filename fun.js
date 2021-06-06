8aa9367a7dd0653d9f9c802198e2d5ee5350b7fb
Zarkozver <49551105+Zarkozver@users.noreply.github.com>
2021-06-06 22:54:08 +0200

    Add files via upload

---
+++ fun.js
@@ -0,0 +1,9 @@
+﻿function Pusti(instrument) {
+    var p = document.getElementById(instrument);
+    p.play();
+}
+function Zaustavi(instrument) {
+    var p = document.getElementById(instrument);
+    p.pause();
+    p.currentTime = 0;
+}
